1776f20218be6c37ded39346065a586d9a94c327
François Best <github@francoisbest.com>
2025-02-24 21:57:40 +0100

    chore: Theme control

--- apps/demo-next/app/layout.tsx
+++ apps/demo-next/app/layout.tsx
@@ -1,5 +1,5 @@
-import { Providers } from '@/components/providers'
 import '@workspace/ui/globals.css'
+import { ThemeProvider } from '@workspace/ui/lib/theme-provider'
 
 export default function RootLayout({
   children,
@@ -9,7 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="antialiased">
-        <Providers>{children}</Providers>
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   )
--- packages/ui/src/lib/theme-provider.tsx
+++ packages/ui/src/lib/theme-provider.tsx
@@ -1,9 +1,7 @@
-"use client"
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import * as React from 'react'
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-
-export function Providers({ children }: { children: React.ReactNode }) {
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
