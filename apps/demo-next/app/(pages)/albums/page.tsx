50b027f44f424bd688b4e058a1e250c1c6b93e77
François Best <github@francoisbest.com>
2025-03-03 22:19:46 +0100

    chore: Add page props stub

--- apps/demo-next/app/(pages)/albums/page.tsx
+++ apps/demo-next/app/(pages)/albums/page.tsx
@@ -2,7 +2,9 @@ import { findAlbums } from '@workspace/db/queries'
 import { AlbumGrid } from '@workspace/ui/demos/albums/album-grid'
 import { Album } from '@workspace/ui/views/demos/albums/album'
 
-export default async function AlbumsPage() {
+type PageProps = {}
+
+export default async function AlbumsPage({}: PageProps) {
   const albums = await findAlbums()
   return (
     <>
