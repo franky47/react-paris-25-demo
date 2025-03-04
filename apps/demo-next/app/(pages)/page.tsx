33a5dea7a6e0da2522d484ae1033f0a4e0881e29
François Best <github@francoisbest.com>
2025-03-04 10:58:58 +0100

    chore: Add links to albums demo for quick access

--- apps/demo-next/app/(pages)/page.tsx
+++ apps/demo-next/app/(pages)/page.tsx
@@ -1,12 +1,20 @@
 import { CounterButton } from '@workspace/ui/views/demos/counter/counter-button'
+import Link from 'next/link'
 import { Suspense } from 'react'
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center mt-12">
-      <Suspense>
-        <CounterButton />
-      </Suspense>
-    </div>
+    <>
+      <div className="flex items-center justify-center mt-12">
+        <Suspense>
+          <CounterButton />
+        </Suspense>
+      </div>
+      <nav className="mt-24 text-center">
+        <Link href="/albums" className="underline text-gray-500">
+          SSR demo
+        </Link>
+      </nav>
+    </>
   )
 }
--- apps/demo-react-router/app/routes/index.tsx
+++ apps/demo-react-router/app/routes/index.tsx
@@ -1,9 +1,17 @@
 import { CounterButton } from '@workspace/ui/views/demos/counter/counter-button'
+import { Link } from 'react-router'
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center mt-12">
-      <CounterButton />
-    </div>
+    <>
+      <div className="flex items-center justify-center mt-12">
+        <CounterButton />
+      </div>
+      <nav className="mt-24 text-center">
+        <Link to="/albums" className="underline text-gray-500">
+          SSR demo
+        </Link>
+      </nav>
+    </>
   )
 }
