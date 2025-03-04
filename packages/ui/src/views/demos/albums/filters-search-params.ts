2abc441dfc6b70939be376aa2da22550e2536c97
François Best <github@francoisbest.com>
2025-03-04 10:59:10 +0100

    chore: Start from scatch in search params defs

--- packages/ui/src/views/demos/albums/filters-search-params.ts
+++ packages/ui/src/views/demos/albums/filters-search-params.ts
@@ -1,7 +1,2 @@
-import { parseAsInteger, parseAsString } from 'nuqs/server'
-
 // Describe the search params we accept
-export const filtersSearchParams = {
-  query: parseAsString.withDefault(''),
-  releaseYear: parseAsInteger,
-}
+export const filtersSearchParams = {}
